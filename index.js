1ef9a87cd3d0740c384ae3be8b48533afac73999
unknown <rmazov@hotmail.com>
2022-09-23 08:46:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { suma, eleva, nombre } from './modulos/matematicas.js'
+import * as moduloMatematicas from './modulos/matematicas.js'
+import getAutor, { libro } from './modulos/literatura.js'
+import chalk from 'chalk';
+
+const sum = moduloMatematicas.suma(4, 12)
+console.log(sum)
+
+const potencia = moduloMatematicas.eleva(2, 21)
+console.log(potencia)
+
+console.log(moduloMatematicas.nombre)
+
+console.log(getAutor())
+console.log(libro)
+console.log(chalk.green(potencia))
